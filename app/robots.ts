0bb0acc7a6bea1ca074d979d74187ab6739bd2b1
Mahmoud <byboldbrain@gmail.com>
2026-07-17 22:43:50 +0300

    Fix robots rules for public crawling

--- app/robots.ts
+++ app/robots.ts
@@ -1,31 +1,38 @@
 import type { MetadataRoute } from 'next'
 import { SITE_URL } from '@/src/lib/site'
 
+const PRIVATE_PATHS = [
+  '/api/',
+  '/admin/',
+  '/account/',
+  '/account-login/',
+  '/login/',
+  '/signup/',
+  '/waiting-list/',
+]
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
         userAgent: '*',
         allow: '/',
-        disallow: [
-          '/api/',
-          '/_next/',
-        ],
+        disallow: PRIVATE_PATHS,
       },
       {
         userAgent: 'Googlebot',
         allow: '/',
-        disallow: ['/api/'],
+        disallow: PRIVATE_PATHS,
       },
       {
         userAgent: 'Bingbot',
         allow: '/',
-        disallow: ['/api/'],
+        disallow: PRIVATE_PATHS,
       },
       {
         userAgent: 'OAI-SearchBot',
         allow: '/',
-        disallow: ['/api/'],
+        disallow: PRIVATE_PATHS,
       },
     ],
     sitemap: `${SITE_URL}/sitemap.xml`,
